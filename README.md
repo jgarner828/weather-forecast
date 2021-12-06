8c4b5563caa5cfd02c50f7340aaa4305bafa7c9e
Justin Garner <j.garner828@gmail.com>
2021-12-06 12:19:55 -0500

    setting up API

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Weather Dashboard App
 -search a city for the local weather!
 - Justin Garner 
 - 12/6/2021
+
+-using https://openweathermap.org/api/one-call-api
---
+++ script.js
@@ -0,0 +1,4 @@
+// The API is called VIA.... https://api.openweathermap.org/data/2.5/onecall?lat=33.748997&lon=-84.387985&appid=af25342231667911f3ebe786f4537f3d
+
+
+// https://openweathermap.org/api/one-call-api#history-how
